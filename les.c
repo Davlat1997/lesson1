2e468947ef36252d73111bc24426be018464c811
Davlat1997 <aslamovavazira818@gmail.com>
2024-12-11 09:38:50 +0500

    6 - misol bajarildi.

--- les.c
+++ les.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
 
-    char text[1000], c;
-    int length, max = 0;
+void find_word(char arr[], char arr2[]) {
+    int i, j, found = 0;
 
-    fgets(text, sizeof(text), stdin);
+   
+    arr2[strcspn(arr2, "\n")] = '\0';
 
-    length = strlen(text);
-
-    c = text[0];
-    for(int i = 0; i < length; i++)
-        if(c == text[i])
-            max++;
-
-    for(int i = 1; i < length; i++){
-        int count = 0;
-
-        for(int j = 0; j < length; j++){
-            if(text[i] == text[j]){
-                count++;
+    
+    for (i = 0; arr[i] != '\0'; i++) {
+       
+        for (j = 0; arr2[j] != '\0'; j++) {
+            if (arr[i + j] != arr2[j]) {
+                break;
             }
         }
-        if(max < count){
-            max = count;
-            c = text[i];
+
+       
+        if (arr2[j] == '\0') {
+            found = 1;
+            break;
         }
     }
 
-    printf("Eng ko’p qatnashgan harf ‘%c’, %d marta\n", c, max);
+    if (found) {
+        printf("Bu so'z seriyada mavjud: %s\n", arr2);
+    } else {
+        printf("Bu so'z seriyada mavjud emas\n");
+    }
+}
+
+int main() {
+    char arr[100], arr2[100];
+
+    printf("Matnni kiriting:\n");
+    fgets(arr, sizeof(arr), stdin);
+
+    printf("So'zni kiriting:\n");
+    fgets(arr2, sizeof(arr2), stdin);
+
+  
+    find_word(arr, arr2);
 
     return 0;
 }
